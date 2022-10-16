1b332856fc3377fe3be89307cdaeefb6128d5777
Anthony Hansord <tonyhansorddev@gmail.com>
2022-10-17 07:06:53 +1100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(array, message) {
+  const birthdayMessages = [];
+  for (let i = 0; i < array.length; i++) {
+    birthdayMessages.push(
+      `Thank you, ${array[i]}, for the wonderful ${message} gift!`
+    );
+    debugger;
+  }
+  return birthdayMessages;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
